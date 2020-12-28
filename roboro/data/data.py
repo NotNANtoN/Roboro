Unified target net updating into Policy Superclass. Also moved done_flag calculation into the calculate_gammas function - this fixed a bug that lead soft_q, munch_q and iqn to not use a discount factor. Added munch_iqn policy config

roboro/data/data.py
@@ -41,15 +41,6 @@ class RLDataModule(pl.LightningDataModule):
         #if val_ds is not None:
         #    self.val_dl = create_dl(val_ds)
 
-    def collate(self, batch):
-        print(batch)
-        print(len(batch))
-        quit()
-
-    def _dataloader(self, ds) -> DataLoader:
-        return torch.utils.data.DataLoader(ds, batch_size=self.batch_size, num_workers=self.num_workers)
-        #, collate_fn=self.collate)
-
     def train_dataloader(self) -> DataLoader:
         """Get train loader"""
         # TODO: combine replay buffer dataloader with expert data dataloader
@@ -62,6 +53,15 @@ class RLDataModule(pl.LightningDataModule):
     def test_dataloader(self) -> DataLoader:
         """Get test loader"""
         return self.test_dl
+
+    def collate(self, batch):
+        print(batch)
+        print(len(batch))
+        quit()
+
+    def _dataloader(self, ds) -> DataLoader:
+        return torch.utils.data.DataLoader(ds, batch_size=self.batch_size, num_workers=self.num_workers)
+        #, collate_fn=self.collate)
 
     def get_train_env(self):
         return self.train_env, self.train_obs

roboro/policies.py
@@ -31,13 +31,25 @@ class Policy(torch.nn.Module):
     def __init__(self, gamma):
         super().__init__()
         self.gamma = gamma
+        self.nets = []
+        self.target_nets = []
 
-    def _calc_gammas(self, extra_info):
+    def _calc_gammas(self, done_flags, extra_info):
+        """Apply the discount factor. If a done flag is set we discount by 0."""
+        gammas = (~done_flags.unsqueeze(-1)).float()
         if "n_step" in extra_info:
-            gammas = self.gamma ** extra_info["n_step"]
+            gammas *= self.gamma ** extra_info["n_step"]
         else:
-            gammas = self.gamma
-        return gammas
+            gammas *= self.gamma
+        return gammas.squeeze()
+
+    def update_target_nets_hard(self):
+        for net, target_net in zip(self.nets, self.target_nets):
+            copy_weights(net, target_net)
+
+    def update_target_nets_soft(self, val):
+        for net, target_net in zip(self.nets, self.target_nets):
+            polyak_update(net, target_net, val)
 
 
 class Q(Policy):
@@ -47,6 +59,9 @@ class Q(Policy):
         self.q_net = MLP(obs_shape, act_shape, **net)
         self.q_net_target = MLP(obs_shape, act_shape, **net)
         freeze_params(self.q_net_target)
+        # Create net lists to update target nets
+        self.nets = [self.q_net]
+        self.target_nets = [self.q_net_target]
         # Decide on using double Q-learning by choosing which method to use to calc next state value:
         if double_q:
             self._next_state_func = self._calc_next_obs_q_vals_double_q
@@ -66,39 +81,33 @@ class Q(Policy):
 
     @torch.no_grad()
     def calc_target_val(self, obs, actions, rewards, done_flags, next_obs, extra_info):
-        q_vals_next = self._next_state_func(next_obs, done_flags)
+        q_vals_next = self._next_state_func(next_obs)
         assert q_vals_next.shape == rewards.shape
-        gammas = self._calc_gammas(extra_info)
+        gammas = self._calc_gammas(done_flags, extra_info)
         targets = rewards + gammas * q_vals_next
         return targets
 
-    def update_target_nets_hard(self):
-        copy_weights(self.q_net, self.q_net_target)
-
-    def update_target_nets_soft(self, val):
-        polyak_update(self.q_net, self.q_net_target, val)
-
     @torch.no_grad()
-    def _q_pred_next_state(self, next_obs, done_flags, net):
+    def _q_pred_next_state(self, next_obs, net):
         """ Specified in function to be potentially overriden by subclasses"""
-        return net(next_obs) * (~done_flags.unsqueeze(-1))
+        return net(next_obs)
 
-    def _calc_next_obs_q_vals(self, next_obs, done_flags):
+    def _calc_next_obs_q_vals(self, next_obs):
         """Calculate the value of the next state via the target network.
         If a done_flag is set the next obs val is 0, else calculate it"""
-        q_vals_next = self._q_pred_next_state(next_obs, done_flags, self.q_net_target)
+        q_vals_next = self._q_pred_next_state(next_obs, self.q_net_target)
         q_vals_next = torch.max(q_vals_next, dim=1)[0]
         return q_vals_next
         
-    def _calc_next_obs_q_vals_double_q(self, next_obs, done_flags):
+    def _calc_next_obs_q_vals_double_q(self, next_obs):
         """Calculate the value of the next state according to the double Q learning rule.
         It decouples the action selection (done via online network) and the action evaluation (done via target network).
         """
         # Next state action selection
-        q_vals_next_online_net = self._q_pred_next_state(next_obs, done_flags, self.q_net)
+        q_vals_next_online_net = self._q_pred_next_state(next_obs, self.q_net)
         max_idcs = torch.max(q_vals_next_online_net, dim=1)[1]
         # Next state action evaluation
-        q_vals_next_target_net = self._q_pred_next_state(next_obs, done_flags, self.q_net_target)
+        q_vals_next_target_net = self._q_pred_next_state(next_obs, self.q_net_target)
         q_vals_next = q_vals_next_target_net.gather(dim=1, index=max_idcs.unsqueeze(1)).squeeze()
         return q_vals_next
 
@@ -119,8 +128,8 @@ class SoftQ(Q):
         self.l0 = l0
 
     @torch.no_grad()
-    def _q_pred_next_state(self, next_obs, done_flags, net):
-        next_state_q_vals = super()._q_pred_next_state(next_obs, done_flags, net)
+    def _q_pred_next_state(self, next_obs, net):
+        next_state_q_vals = super()._q_pred_next_state(next_obs, net)
         next_state_policy_distr = torch.softmax(next_state_q_vals, dim=1)
         next_state_preds = next_state_q_vals - self._calc_entropy(next_state_q_vals)
         return (next_state_policy_distr * next_state_preds).mean(dim=1).unsqueeze(-1)
@@ -154,6 +163,9 @@ class V(Policy):
         self.v_net = MLP(obs_shape, v_out_size, **net)
         self.v_net_target = MLP(obs_shape, v_out_size, **net)
         freeze_params(self.v_net_target)
+        # Create net lists to update target nets
+        self.nets = [self.v_net]
+        self.target_nets = [self.v_net_target]
         
     def forward(self, obs):
         return self.v_net(obs)
@@ -168,25 +180,19 @@ class V(Policy):
 
     @torch.no_grad()
     def calc_target_val(self, obs, actions, rewards, done_flags, next_obs, extra_info):
-        v_vals_next = self._calc_next_obs_vals(next_obs, done_flags)
-        gammas = self._calc_gammas(extra_info)
+        v_vals_next = self._calc_next_obs_vals(next_obs)
+        gammas = self._calc_gammas(done_flags, extra_info)
         targets = rewards + gammas * v_vals_next
         return targets
 
-    def update_target_nets_hard(self):
-        copy_weights(self.v_net, self.v_net_target)
-
-    def update_target_nets_soft(self, val):
-        polyak_update(self.v_net, self.v_net_target, val)
-
     @torch.no_grad()
-    def _calc_next_obs_vals(self, next_obs, done_flags):
+    def _calc_next_obs_vals(self, next_obs):
         """If a done_flag is set the next obs val is 0, else calculate it"""
-        v_vals_next = self.v_net_target(next_obs) * (~done_flags.unsqueeze(-1))
+        v_vals_next = self.v_net_target(next_obs)
         return v_vals_next.squeeze()
 
 
-class QV(torch.nn.Module):
+class QV(Policy):
     """Train a state-action value network (Q) network and an additional state value network (V) and
     train the Q net using the V net.
     """
@@ -195,6 +201,9 @@ class QV(torch.nn.Module):
         self.v = V(obs_shape, gamma, net=net)
         self.q = Q(obs_shape, act_shape, gamma, double_q, net=net)
         # TODO: if dueling is set, try to incorporate the V net into the Q net
+        # Create net lists to update target nets
+        self.nets = [self.q.q_net, self.v.v_net]
+        self.target_nets = [self.q.q_net_target, self.v.v_net_target]
 
     def forward(self, obs):
         return self.q(obs)
@@ -203,14 +212,6 @@ class QV(torch.nn.Module):
         v_target = self.v.calc_target_val(*loss_args)
         loss = self._calc_qv_loss(*loss_args, q_target=v_target, v_target=v_target)
         return loss
-        
-    def update_target_nets_hard(self):
-        self.v.update_target_nets_hard()
-        self.q.update_target_nets_hard()
-
-    def update_target_nets_soft(self, val):
-        self.v.update_target_nets_soft(val)
-        self.q.update_target_nets_soft(val)
 
     def _calc_qv_loss(self, *loss_args, q_target=None, v_target=None):
         v_loss = self.v.calc_loss(*loss_args, targets=v_target)
@@ -257,6 +258,9 @@ class IQN(SoftQ):
         self.q_net = IQNNet(state_size, action_size, num_quantiles, **net)
         self.q_net_target = IQNNet(state_size, action_size, num_quantiles, **net)
         freeze_params(self.q_net_target)
+        # Create net lists to update target nets
+        self.nets = [self.q_net]
+        self.target_nets = [self.q_net_target]
 
     def calc_loss(self, obs, actions, rewards, done_flags, next_obs, extra_info, targets=None):
         batch_size = obs.shape[0]
@@ -281,7 +285,7 @@ class IQN(SoftQ):
         batch_size = obs.shape[0]
         q_quants_next, _ = self.q_net_target.get_quantiles(next_obs, self.num_quantiles)
         exp_q_next = q_quants_next.mean(dim=1)
-        gammas = self._calc_gammas(extra_info)
+        gammas = self._calc_gammas(done_flags, extra_info).unsqueeze(-1).unsqueeze(-1)
         if not self.munchausen:
             # Get max predicted Q values (for next states) from target model
             action_idx = torch.argmax(exp_q_next, dim=1, keepdim=True)
@@ -290,15 +294,15 @@ class IQN(SoftQ):
             # Take max actions
             q_targets_next = q_quants_next.gather(dim=2, index=action_idx).transpose(1, 2)
             # Compute Q targets for current states
-            q_targets = rewards.unsqueeze(-1) + (gammas * q_targets_next * (~done_flags))
+            q_targets = rewards.unsqueeze(-1) + (gammas * q_targets_next)
         else:
             # calculate log-pi
             tau_log_pi_next = self._calc_entropy(exp_q_next).unsqueeze(1)
 
             pi_target = torch.softmax(exp_q_next / self.tau, dim=1).unsqueeze(1)
-            next_state_vals = (gammas *
-                         (pi_target * (q_quants_next - tau_log_pi_next) * (~done_flags)).sum(2)
-                        ).unsqueeze(1)
+            next_state_vals = (gammas.squeeze(-1) *
+                               (pi_target * (q_quants_next - tau_log_pi_next)).sum(2)
+                               ).unsqueeze(1)
             assert next_state_vals.shape == (batch_size, 1, self.num_quantiles)
 
             q_k_target = self.q_net_target(obs).detach()